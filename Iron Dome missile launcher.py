8704bc6c34ec6e200eed5db41ab124bb695c1cbd
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-07 06:12:07 +0500

    Create Iron Dome missile launcher.py

---
+++ Iron Dome missile launcher.py
@@ -0,0 +1,25 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+for num in range(4, 20):
+    if num % 4 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number", num)
+
+    
+Found an even number 4
+Found an odd number 5
+Found an odd number 6
+Found an odd number 7
+Found an even number 8
+Found an odd number 9
+Found an odd number 10
+Found an odd number 11
+Found an even number 12
+Found an odd number 13
+Found an odd number 14
+Found an odd number 15
+Found an even number 16
+Found an odd number 17
+Found an odd number 18
+Found an odd number 19

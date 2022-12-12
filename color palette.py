19bd85322643e7ad7b245c4f7343865716ef9a6d
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-12 19:35:51 +0500

    Create color palette.py

---
+++ color palette.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+'{:_}'.format(1000000)
+'1_000_000'
+'{:_x}'.format(0xFFFFFFFF)
+'ffff_ffff'

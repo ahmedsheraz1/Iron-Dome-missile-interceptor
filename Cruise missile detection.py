e5b0804dffc9806df7a00276aa9f32b8453fe3f8
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-12 17:08:49 +0500

    Create Cruise missile detection.py

---
+++ Cruise missile detection.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from decimal import *
+round(Decimal('0.70') * Decimal('1.05'), 2)
+Decimal('0.74')
+round(.70 * 1.05, 2)
+0.73
